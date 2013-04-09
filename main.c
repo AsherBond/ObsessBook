b6b6797c48b0351fb3faa7b64480a7d62059a0a3
Mateusz Adamowski <mateusz@adamowski.pl>
2013-04-09 10:14:43 +0200

    reorder lines

--- main.c
+++ main.c
@@ -8,19 +8,14 @@ int main( argc, argv )
     int const argc;
     char const * const * const argv;
 {
-    FILE *f;
-
-    f = fopen( "INPUT.TXT", "r" );
-
     userdb * OBook;
 
+    FILE *f;
+    f = fopen( "INPUT.TXT", "r" );
     OBook = userdb_load( f );
+    fclose( f );
 
     userdb_show( OBook );
-
-
-
-    fclose( f );
 
     printf( "[%d]\n", DERPCON(
         userdb_find_by_id( OBook, 2002 ),
